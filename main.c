b86286989d6537b4c86eb31b12b0396efe7a331b
NishaRV <79297680+NishaRV@users.noreply.github.com>
2021-03-01 21:28:01 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
-int main() {
-   int n=6;
-   int sum = 0;
-   for (int i = 1; i <= n; i++)
-      sum += (2*i - 1) * (2*i - 1);
-   printf("The sum of square of first %d odd numbers is %d",n, sum);
-   return 0;
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("Enter a number:");
+	scanf("%d",&n);
+	printf("Even numbers till %d:\n",n);
+	for(i=2;i<=n;i=i+2)
+	{
+		printf("%d ",i);
+	}
+	return 0;
 }
+
