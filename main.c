5049ee7d300ed762f094140244bbc5b6f5b4720d
NishaRV <79297680+NishaRV@users.noreply.github.com>
2021-03-01 21:29:11 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 int main()
 {
-	int n,i;
-	printf("Enter a number:");
-	scanf("%d",&n);
-	printf("Even numbers till %d:\n",n);
-	for(i=2;i<=n;i=i+2)
+	int num1,num2,num3;
+	printf("Enter three numbers:");
+	scanf("%d%d%d",&num1,&num2,&num3);
+	if(num1 < num2 && num1 < num3)
 	{
-		printf("%d ",i);
+		printf("%d is smallest",num1);
+	}
+	else if(num2 < num3)
+	{
+		printf("%d is smallest",num2);
+	}
+	else
+	{
+		printf("%d is smallest",num3);
 	}
 	return 0;
 }
-
